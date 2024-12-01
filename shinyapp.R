9978e15fad47315c2cf2f7c74a7a66b56a631ce2
Chaeyeong <chaeyeong@uchicago.edu>
2024-12-01 15:25:36 -0600

    updated

--- shinyapp.R
+++ shinyapp.R
@@ -3,18 +3,18 @@ library(sf)
 library(shiny)
 library(plotly)
 
-# Create a world map to select a country and year and visualize how financial inclusion for the poor correlates
-# with poverty in the world over time
+# Create bar graphs to select a country and year and visualize how financial inclusion 
+# for the poor correlates with poverty
 ## Define ui
-ui <- fluidPage(
-  titlePanel("Correlation Between Financial Inclusion for the Poor and Poverty in the World Over time"),
+ui_1 <- fluidPage(
+  titlePanel("Financial Inclusion for the Poor vs Poverty"),
   sidebarLayout(
     sidebarPanel(
       tags$img(src = "https://d11jve6usk2wa9.cloudfront.net/platform/10747/assets/logo.png",
-               height = 90,
-               width = 260),
-      selectInput(inputId = "year_input", label = "Year", choices = NULL),
-      selectInput(inputId = "country_input", label = "Country", choices = NULL)
+               height = 80,
+               width = 220),
+      selectInput(inputId = "country_input", label = "Country", choices = NULL),
+      selectInput(inputId = "year_input", label = "Year", choices = NULL)
     ),
     mainPanel(
       plotlyOutput("country_plot")
@@ -23,52 +23,147 @@ ui <- fluidPage(
 )
 
 ## Define server logic
-server <- function(input, output, session) {
+server_1 <- function(input, output, session) {
   inclusive_poverty_gdp <- read_csv("data/inclusive_poverty_gdp.csv")
-  zip_codes <- st_read("geo_export_fb313970-5c89-4a00-8f7c-2f7e7ca8255d.shp")
-  public_schools <- public_schools %>%
-    select(`Safety Score`, `Name of School`, LONGITUDE, LATITUDE)
-  public_schools_sf <- st_as_sf(public_schools, coords = c("LONGITUDE", "LATITUDE"), crs = st_crs(zip_codes), remove = FALSE)
-  public_schools_sf <- filter(public_schools_sf, !is.na(`Safety Score`))
+  inclusive_poverty_gdp_long <- inclusive_poverty_gdp %>%
+    pivot_longer(cols = starts_with("per_"), names_to = "metric", values_to = "value")
   
-  
-  ## Subset based on user-inputted safety score
-  public_schools_sf_updated <- reactive({
-    public_schools_sf %>% filter(`Safety Score` >= as.numeric(input$safety_input))
+  observe({
+    country_choices <- unique(inclusive_poverty_gdp_long$`country name`)
+    updateSelectInput(session, "country_input", choices = country_choices)
   })
   
-  ## call the resulting dataframe public_schools_sf_updated
-  observeEvent(public_schools_sf_updated(), {
-    school_choices <- unique(public_schools_sf_updated()$`Name of School`)
-    updateSelectInput(inputId = "school_input", choices = c("All Schools" = "", school_choices))
+  ### Reactive filtered data based on user inputs
+  filtered <- reactive({
+    data <- inclusive_poverty_gdp_long
+    
+    # Filter by country
+    if (!is.null(input$country_input) && input$country_input != "") {
+      data <- data[data$`country name` == input$country_input, ]
+    }
+    
+    # Filter by year
+  if (!is.null(input$year_input) && input$year_input != "") {
+    data <- data[data$year == as.numeric(input$year_input), ]
+  }
+    print(data)
+    data
   })
   
-  ## Render the plot
-  output$school_plot <- renderPlotly({
+  ### call the resulting dataframe filtered
+  observeEvent(input$country_input, {
+    filtered_data <- inclusive_poverty_gdp_long[inclusive_poverty_gdp_long$`country name` == input$country_input, ]
+    year_choices <- unique(filtered_data$year)
+    updateSelectInput(session, "year_input", choices = c("All Years" = "", year_choices))
+  })
+  
+  ### Render the plot
+  output$country_plot <- renderPlotly({
+    plot_data <- filtered()
+    
+    if (nrow(plot_data) == 0) {
+      return(NULL)
+    }
+    
+    ggplotly(
+      ggplot(data = plot_data) +
+        geom_point(aes(poverty_headcount_ratio, value, fill = metric)) +
+        labs(
+          title = paste("Financial Inclusion for", input$country_input, "in", input$year_input),
+          x = "Poverty Headcount Ratio",
+          y = "Financial Inclusion for income, poorest 40% (% ages 15+)",
+          caption = "Source: World Bank"
+        ) +
+        theme_minimal() +
+        theme(
+          axis.title.x = element_text(size = 10),
+          axis.title.y = element_text(size = 10) 
+        )
+    )
+  })
+}
+
+shinyApp(ui = ui_1, server = server_1)
+
+# Create a world map to filter income group (low, middle, high-income level) 
+# and compare financial inclusion across wealthier and poorer countries for the poor
+## Define ui
+ui_2 <- fluidPage(
+  titlePanel("Financial Inclusion across Income Level in the World"),
+  sidebarLayout(
+    sidebarPanel(
+      tags$img(src = "https://d11jve6usk2wa9.cloudfront.net/platform/10747/assets/logo.png",
+               height = 80,
+               width = 220),
+      selectInput(inputId = "income_input", label = "Income Level", choices = NULL),
+      selectInput(inputId = "metric_input", label = "Metric", choices = NULL),
+    ),
+    mainPanel(
+      plotlyOutput("income_plot")
+    )
+  )
+)
+
+## Define server logic
+server_2 <- function(input, output, session) {
+  world_map <- st_read("data/ne_10m_admin_0_countries.shp")
+  inclusive_poverty_gdp_sf <- st_as_sf(inclusive_poverty_gdp_long, 
+                                       coords = c("longitude", "latitude"), 
+                                       crs = st_crs(world_map), remove = FALSE)
+  
+  observe({
+    income_choices <- unique(inclusive_poverty_gdp_sf$`income group`)
+    updateSelectInput(session, "income_input", choices = income_choices)
+  })
+  
+  ### Reactive filtered data based on user inputs
+  filtered <- reactive({
+    data <- inclusive_poverty_gdp_sf
+    
+    # Filter by income level
+    if (!is.null(input$income_input) && input$income_input != "") {
+      data <- data[data$`income group` == input$income_input, ]
+    }
+    
+    # Filter by year
+    if (!is.null(input$metric_input) && input$metric_input != "") {
+      data <- data[data$metric == input$metric_input, ]
+    }
+    print(data)
+    data
+  })
+  
+  ### call the resulting dataframe filtered
+  observeEvent(input$income_input, {
+    filtered_data <- inclusive_poverty_gdp_sf[inclusive_poverty_gdp_sf$`income group` == input$income_input, ]
+    metric_choices <- unique(filtered_data$metric)
+    updateSelectInput(session, "metric_input", choices = c("All Metrics" = "", metric_choices))
+  })
+  
+  ### Render the plot
+  output$country_plot <- renderPlotly({
     ggplot() +
-      geom_sf(data = zip_codes) +
+      geom_sf(data = world_map) +
       geom_sf(
-        data = if (input$school_input == "") {
-          public_schools_sf_updated() 
+        data = if (input$income_input == "") {
+          filtered() 
         } else {
-          public_schools_sf_updated() %>% filter(`Name of School` == input$school_input)  
+          filtered() %>% filter(`income group` == input$income_input)  
         },
-        aes(fill = `Safety Score`), shape = 21, size = 2
+        aes(fill = value), shape = 21, size = 2
       ) +
       scale_fill_gradient(low = "white", high = "blue") +
       labs(
-        title = "Location of Public School by Safety Scores and School Name in Chicago",
+        title = "Financial Inclusion acoss Income Level in the World",
         x = "Longitude", y = "Latitude",
-        fill = "Safety Score",
-        caption = "Source: City of Chicago"
+        fill = "Percent",
+        caption = "Source: World Bank"
       ) +
       theme_minimal()
   })
 }
 
-# Run the app
-shinyApp(ui = ui, server = server)
-
-
-# Create a world map to filter income group (low, middle, high-income level) 
-# and compare financial inclusion gaps across wealthier and poorer countries for the poor
+## Run the app
+shinyApp(ui = ui_2, server = server_2)
+inclusive_poverty_gdp_sf
+world_map
--- staticplot.R
+++ staticplot.R
@@ -5,10 +5,7 @@ style_file("/Users/yeong/Documents/GitHub/R-II_final/staticplot.R")
 inclusive_poverty_gdp <- read_csv("data/inclusive_poverty_gdp.csv")
 colnames(inclusive_poverty_gdp)
 
-# Creating a plot financial inclusion for the poor vs poverty headcount ratio across years
-inclusive_poverty_gdp_long <- inclusive_poverty_gdp %>%
-  pivot_longer(cols = starts_with("per_"), names_to = "metric", values_to = "value")
-
+# Creating a plot estimating financial inclusion by poverty headcount ratio across years
 plot_financial_inclusion_by_poverty <- inclusive_poverty_gdp_long %>%
   ggplot(aes(x = poverty_headcount_ratio, y = value, color = metric)) +
   geom_smooth(method = "lm", se = FALSE, size = 1) +
@@ -16,11 +13,47 @@ plot_financial_inclusion_by_poverty <- inclusive_poverty_gdp_long %>%
   labs(
     title = "Trends in Financial Inclusion vs Poverty across Years",
     x = "Poverty Headcount Ratio",
-    y = "Financial Inclusion for income, poorest 40% (% ages 15+)",
+    y = "Estimated Financial Inclusion (from LM)",
     color = "Metric"
+  ) +
+  theme_minimal() +
+  theme(
+    strip.text = element_text(size = 10),
+    plot.title = element_text(size = 12, face = "bold"),
+    axis.title = element_text(size = 10),
+    legend.position = "bottom",
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 7),
+    legend.key.size = unit(0.5, "cm")
+  ) +
+  guides(
+    color = guide_legend(nrow = 2, byrow = TRUE)
+  )
+
+ggsave("plot_financial_inclusion_by_poverty.png", plot = plot_financial_inclusion_by_poverty, path = "images")
+
+# Creating choropleth for financial inclusion in the world
+world_map <- st_read("data/ne_10m_admin_0_countries.shp")
+inclusive_poverty_gdp_sf <- st_as_sf(inclusive_poverty_gdp_long,
+  coords = c("longitude", "latitude"),
+  crs = st_crs(world_map), remove = FALSE
+)
+
+choropleth_financial_inclusion <- ggplot() +
+  geom_sf(data = world_map) +
+  geom_sf(data = inclusive_poverty_gdp_sf, aes(fill = value), shape = 21, size = 2) +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(
+    title = "Choropleth for Financial Inclusion in the World",
+    fill = "Percent",
+    x = "Longitude", y = "Latitude",
+    caption = "Source: World Bank"
+  ) +
+  theme(
+    strip.text = element_text(size = 10, face = "bold"),
+    plot.title = element_text(size = 12, face = "bold"),
+    axis.title = element_text(size = 10)
   ) +
   theme_minimal()
 
-print(plot_financial_inclusion_by_poverty)
-
-# Creating a plot analyzing natural language
+ggsave("choropleth_financial_inclusion.png", plot = choropleth_financial_inclusion, path = "images", width = 10, height = 10)
